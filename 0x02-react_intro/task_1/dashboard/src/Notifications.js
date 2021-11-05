30ae4314f18a329257a365f90d2a85a325d9ecac
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2021-11-05 07:24:24 -0500

    deleting files and folders

--- 0x02-react_intro/task_1/dashboard/src/Notifications.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Notifications.css';
-
-function notifications() {
-  return (<div className="Notifications">Here is the list of notifications</div>)
-}
-
-export default notifications();
-
